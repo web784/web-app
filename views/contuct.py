c89e32121d18a8842cdbbfff0bff45c14968dec5
Nguvukuuzamungu <webacco177@gmail.com>
2025-04-28 19:30:23 +0300

    Add files via upload
    initial commit

--- views/contuct.py
+++ views/contuct.py
@@ -5,8 +5,106 @@ import streamlit as st
 st.set_page_config(page_title="Nguvu Kuu Message", layout="wide")
 
 
+@ st.dialog("INFO")
+def show_contuct_form():
+      st.html("""
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-st.title("MESSAGE WILL BE RESEVD")
+
+          <!-------------- STYLE FOR CONTUCT FORM ---------------->
+
+
+          <style>
+          .contect-container {
+               height: 65vh;
+               display: flex;
+               align-items: center;
+               justify-content: space-evenly;
+               justify-content: space-evenly;
+               justify-content: space-betwen;
+          }
+
+          .contect-left-titel h2 {
+               font-weight: 600;
+               color: #a363aa;
+               font-size: 40px;
+               margin-bottom: 5px;
+          }
+          .contect-inputs {
+               width: 300px;
+               border: none;
+               outline: none;
+               padding: 15px;
+               font-weight: 300;
+               color: white;
+               border-radius: 50px;
+               justify-content: space-evenly;
+               background-color: transparent;
+               border: 2px solid #ff994f;
+          }
+
+          .contect-left textarea {
+               height: 160px;
+               padding: 30px;
+               border-radius: 20px;
+               border: 2px solid #ff994f;
+          }
+          .contect-inputs::placeholder {
+               color: #a9a9a9;
+               font-family: Arial, Helvetica, sans-serif;
+          }
+
+          .j {
+               display: flex;
+               align-items: center;
+               padding: 15px 30px;
+               font-size: 16px;
+               color: #fff;
+               gap: 10px;
+               border: none;
+               border-radius: 50px;
+               background: linear-gradient(270deg, #ff994f, #fa6d86);
+               cursor: pointer;
+          }
+
+          .j {
+               text-decoration: none;
+          }
+          </style>
+
+          <!-----------  CONTUCT FORM  -------------->
+
+          </head>
+
+          <body>
+
+
+          <div class="contect-container" id="contuct">
+          <form form action="https://api.web3forms.com/submit" method="POST" class="contect-left">
+          <div class="contect-left-titel">
+          <h2>SEND YOUR MESSAGE</h2>
+          </div>
+          <input type="hidden" name="access_key" value="2d3176aa-a029-4004-aaa2-ba6770d84d40">
+          <input type="text" name="name" placeholder="your name" class="contect-inputs" required>
+          <input type="text" name="email" placeholder="your email" class="contect-inputs" required>
+          <textarea name="message" placeholder="your message" class="contect-inputs required"></textarea>
+          <button type="submit" class="j">submit</button>
+          </form>
+
+
+          </body>
+
+          </html>
+          """)
+
+
+
+
+st.title("SEND YOUR MESSAGE HERE")
 st.write("---")
 
 # response message
@@ -14,115 +112,12 @@ st.write("---")
 st.toast("We Will Responde Through Your Email/Gmail")
 st.toast("Utajibiwa Kupitia Email/Gmail Yako")
 with st.container():
-    st.html("""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     if st.button("SEND HERE"):
+          show_contuct_form()
 
 
-    <!-------------- STYLE FOR CONTUCT FORM ---------------->
 
 
-    <style>
-.contect-container {
-     height: 80vh;
-     display: flex;
-     align-items: center;
-     justify-content: space-evenly;
-}
 
-.contect-left {
-     display: flex;
-     flex-direction: column;
-     align-items: start;
-     gap: 20px;
-}
-
-.contect-left-titel h2 {
-     font-weight: 600;
-     color: #a363aa;
-     font-size: 40px;
-     margin-bottom: 5px;
-}
-.contect-left-titel hr {
-     border: none;
-     width: 120px;
-     height: 5px;
-     background-color: #a363aa;
-     border-radius: 10px;
-     margin-bottom: 20px;
-}
-
-.contect-inputs {
-     width: 700px;
-     border: none;
-     outline: none;
-     padding: 25px;
-     font-weight: 500;
-     color: #fff;
-     border-radius: 50px;
-}
-
-.contect-left textarea {
-     height: 160px;
-     padding: 15px;
-     border-radius: 20px;
-}
-
-.contect-inputs:focus {
-     border: 2px solid #ff994f;
-}
-
-.contect-inputs::placeholder {
-     color: #a9a9a9;
-     font-family: Arial, Helvetica, sans-serif;
-}
-
-.j {
-     display: flex;
-     align-items: center;
-     padding: 15px 30px;
-     font-size: 16px;
-     color: #fff;
-     gap: 10px;
-     border: none;
-     border-radius: 50px;
-     background: linear-gradient(270deg, #ff994f, #fa6d86);
-     cursor: pointer;
-}
-
-.j {
-     text-decoration: none;
-}
-    </style>
-
-<!-----------  CONTUCT FORM  -------------->
-
-</head>
-
-<body>
-
-
-<div class="contect-container" id="contuct">
-<form form action="https://api.web3forms.com/submit" method="POST" class="contect-left">
-<div class="contect-left-titel">
-<h2>SEND YOUR MESSAGE HERE</h2>
-     <hr>
-</div>
-<input type="hidden" name="access_key" value="2d3176aa-a029-4004-aaa2-ba6770d84d40">
-<input type="text" name="name" placeholder="your name" class="contect-inputs" required>
-<input type="text" name="email" placeholder="your email" class="contect-inputs" required>
-<textarea name="message" placeholder="your message" class="contect-inputs required"></textarea>
-<button type="submit" class="j">submit</button>
-</form>
-
-
-</body>
-
-</html>
-    """)
-    st.write("---")
-    st.popover("Enjoy Using Our Website Secuerly")
-    st.popover("Furahia Tovutihii")
+st.status("Enjoy Using Our Website Secuerly")
+st.status("Furahia Tovutihii")
--- views/more.py
+++ views/more.py
@@ -5,47 +5,77 @@ st.set_page_config(page_title="Nguvu Kuu Feedback", layout="wide")
 
 st.title("ABOUT NGUVU KUU ONLINE")
 
-st.title("KUHUSU")
-st.write("""
-KUHUSU KANISA LA NGUVU KUU
-TUNA PATIKANA ARUSHA SANAWARI YA JUU TANZANIA
-
-Tovuti Hii Ya Kanisa La Nguvu Kuu Imetengenezwa Na Kanisa Kwaajili Yakuifikisha Kazi Ya Mungu Kimataifa.
-ajenda yetu dunia nzima imjue mungu
-tutuma Sadaka Yako.
-
-SIMU NAMBA, 0756302908 , 0618102908
-Mungu Atakuzidishia Zaidi Na Zaidi.
-Okoka Leo Nafamilia Yako Muingie Kwa Baba Aitwae Yesu Kristo.
-Amen.
-
-TUNA WEZA KUFANYA LOLOTE KATIKA YEYE ATUTIAE NGUVU.
-
-        """
-)
+st.header("KUHUSU")
 st.write("---")
-st.title("ABOUT")
-st.write("""
 
-We Are The CHILDRENS Of GOD We Have Create This Web For All People From This World In Order Them To Knwo God And Then To Be The One Who Will Go In Heaven
-If You Have Been Savied By God Through This Web site
 
-Don't Forget To Send Your Offer _ _ _
+with st.container():
+    col1_column,col3_column, = st.columns(2, gap="small")
+    with col1_column:
+        with st.container():
+                st.write("KUHUSU KANISA LA NGUVU KUU TUNA PATIKANA ARUSHA SANAWARI YA JUU TANZANIA")
+        with st.container():
+                col1_column,col2_column, = st.columns(2, gap="small")
+                with col1_column:
+                        st.write("""
+                        Tovuti Hii Ya Kanisa La Nguvu Kuu Imetengenezwa Na Kanisa Kwaajili Yakuifikisha Kazi Ya Mungu Kimataifa.
+                        ajenda yetu dunia nzima imjue mungu
+                        tutuma Sadaka Yako.
+                                """
+                )
+                with col2_column:
+                        st.write("""
+                        SIMU NAMBA, 0756302908 , 0618102908
+                        Mungu Atakuzidishia Zaidi Na Zaidi.
+                        Okoka Leo Nafamilia Yako Muingie Mbinguni Njoo Wewe Familia Ndugu Jamaa Na Marafiki
+                        Muokoke Leo Mungu Wetu Ni Wa Rehema Ukiamini Utaokolewa Njoo Leo Utengeneze Kesho Njema
+                        Amen.
 
-Send It Through This Numbers
+                                """
+                )
+        with st.container():
+                st.write("TUNA WEZA KUFANYA LOLOTE KATIKA YEYE ATUTIAE NGUVU.")
+                st.write("""
+                        Uponyaji, Kufunguliwa, Kuokoka, Ni Nguvu Kuu Za Mungu Njoo
+                        Na Bwana Awejuu Yako Ukiamini Utapokea Bali Akunamatokeo 
+                        Yoyote Pasipokuamini.
 
-PHONE NO 0756302908 , 0618102908
 
-We Can Do All This Through Him Gives Us Strength
-PHILP 4:13 
-        """)
+
+
+                        """
+        )
+
+
+
+
+    st.write("---")
+    st.header("ABOUT")
+    st.write("""
+        We Are The CHILDRENS Of GOD We Have Create This Web For All People From This World In Order Them To Knwo God And Then To Be The One Who Will Go In Heaven
+        If You Have Been Savied By God Through This Web site
+
+        Don't Forget To Send Your Offer _ _ _
+
+        Send It Through This Numbers
+
+        PHONE NO 0756302908 , 0618102908
+
+        We Can Do All This Through Him Gives Us Strength
+        PHILP 4:13 
+                """
+        )
+    with col3_column:
+        st.image("./assets/logo2.png", width=200,)
+        st.write("---")
+        st.write("KIKOMBE CHA REHEMA KILICHO ACHILIWA TAREHE 6/4/2025 KWA AJILI YA WATU WOTE")
 
 
 
 st.write("##")
-st.code("[DEVELOPDE By @Webacco With code better] Follow Me At Webacco177@gmail.com")
+st.code("[ DEVELOPDE By @Webacco With code better ] Contuct us At Webacco177@gmail.com")
 st.write("""Webacco From Arusha Tanzania
 
- I Am Website Developer Also Ai Chatbot Developer & etc.
+ We Are Website Developer, Ai Chatbot, Mobile Applications, Online Marketing, Grafics Desine, Post maker & etc.
  
- For Works Like This Pleas Send A Word  'Web' To This Gmail Account Webacco177@gmail.com""")
+ For Works Like This Don't Forget To Contuct Us To This Gmail Account Webacco177@gmail.com""")
